57adbb71186f9ffa0eacdd51a21cb0d2af65b2d3
Thiago Silva <thiagovieiraas@gmail.com>
2016-10-25 08:04:04 -0200

    Generated the first pdf

--- code/dsu.cpp
+++ code/dsu.cpp
@@ -1,37 +1,28 @@
-class UnionFind {
- private:
-  vector<int> p, rank;
-
- public:
-  UnionFind(int N)
-  {
-    rank.assign(N, 0);
-    p.assign(N, 0);
-    for (int i = 0; i < N; i++) p[i] = i;
-  }
-  int findSet(int i)
-  {
-    if (p[i] == i) return i;
-    return p[i] = findSet(p[i]);
-  }
-  bool isSameSet(int i, int j)
-  {
-    if (findSet(i) == findSet(j))
-      return true;
-    else
-      return false;
-  }
-  void unionSet(int i, int j)
-  {
-    if (!isSameSet(i, j)) {
-      int x = findSet(i);
-      int y = findSet(j);
-      if (rank[x] > rank[y])
-        p[y] = x;
-      else {
-        p[x] = y;
-        if (rank[x] == rank[y]) rank[y]++;
-      }
+const int N=500010;
+int p[N],rank[N];
+void init()
+{
+  memset(rank,0,sizeof(rank));
+  for(int i=0;i<N;i++) p[i]=i;
+}
+int findset(int i)
+{
+  if(p[i]==i) return i;
+  return p[i]=findset(p[i]);
+}
+bool same(int i, int j)
+{
+  return (findset(i) == findset(j));
+}
+void unionSet(int i, int j)
+{
+  if (!same(i, j)) {
+    int x = findset(i), y=findset(j);
+  if (rank[x] > rank[y])
+      p[y] = x;
+    else {
+      p[x] = y;
+      if (rank[x] == rank[y]) rank[y]++;
     }
   }
-};
+}
